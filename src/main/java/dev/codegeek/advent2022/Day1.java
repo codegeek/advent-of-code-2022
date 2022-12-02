deef46e6bb4a58d6218dc468d293a077fd17a2c1
Guillermo Castro <39493477+codegeek@users.noreply.github.com>
2022-12-01 18:18:21 -0600

    Advent of Code 2022: Day 1 Part 2

--- src/main/java/dev/codegeek/advent2022/Day1.java
+++ src/main/java/dev/codegeek/advent2022/Day1.java
@@ -3,6 +3,9 @@ package dev.codegeek.advent2022;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.logging.Logger;
@@ -15,6 +18,12 @@ public class Day1 {
         day1.processFile("day1/input");
     }
 
+    public record Elf(List<Integer> calories) {
+        public Integer totalCalories() {
+            return calories.stream().mapToInt(Integer::intValue).sum();
+        }
+    }
+
     public void processFile(String filename) {
         try {
             Path path = Paths.get(Objects.requireNonNull(getClass().getClassLoader()
@@ -22,18 +31,20 @@ public class Day1 {
 
             List<String> lines = Files.readAllLines(path);
 
-            int sumCalories = 0;
-            int maxCalories = 0;
+            final var elves = new HashSet<Elf>();
+            var elfCalories = new ArrayList<Integer>();
             for (String line : lines) {
                 try {
-                    int calories = Integer.parseInt(line);
-                    sumCalories += calories;
+                    elfCalories.add(Integer.parseInt(line));
                 } catch (NumberFormatException nfe) {
-                    maxCalories = Math.max(sumCalories, maxCalories);
-                    sumCalories = 0;
+                    elves.add(new Elf(elfCalories));
+                    elfCalories = new ArrayList<>();
                 }
             }
-            log.info("Max Calories: " + maxCalories);
+
+            var sortedElves = elves.stream().sorted(Comparator.comparing(Elf::totalCalories).reversed()).toList();
+            log.info("Max Calories: " + sortedElves.get(0).totalCalories());
+            log.info("Top three calories (sum): " + sortedElves.stream().limit(3).mapToInt(Elf::totalCalories).sum());
         } catch (Exception e) {
             log.severe("Could not find file: " + filename);
             throw new RuntimeException(e);
