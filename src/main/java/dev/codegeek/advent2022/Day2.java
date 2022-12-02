b19c978a3a5a7f20fd73458c223b84f6c1bdf9b8
Guillermo Castro <39493477+codegeek@users.noreply.github.com>
2022-12-02 00:13:32 -0600

    Advent of Code 2022: Day 2 Part 2

--- src/main/java/dev/codegeek/advent2022/Day2.java
+++ src/main/java/dev/codegeek/advent2022/Day2.java
@@ -16,54 +16,71 @@ public class Day2 {
     public void processFile(String filename) {
         var optionalLines = Util.loadFile(filename);
         if (optionalLines.isPresent()) {
-            int totalScore = 0;
+            int totalScore1 = 0, totalScore2 = 0;
             for(String line : optionalLines.get()) {
                 var choices = line.split(" ");
-                totalScore += calculateScore(choices[0], choices[1]);
+                totalScore1 += calculateScore1(choices[0], choices[1]);
+                totalScore2 += calculateScore2(choices[0], choices[1]);
             }
-            log.info("Total score: " + totalScore);
+            log.info("Total score Part 1: " + totalScore1);
+            log.info("Total score Part 2: " + totalScore2);
         } else {
             log.severe("Could not find file: " + filename);
         }
     }
 
-    public int calculateScore(String opponentChoice, String myChoice) {
+    public int calculateScore1(String opponentChoice, String myChoice) {
         int finalScore = shapeValue.get(myChoice);
         switch (opponentChoice) {
             case "A" -> {
                 switch (myChoice) {
-                    case "X": // Tie
-                        finalScore += 3;
-                        break;
-                    case "Y": // Paper defeats Rock
-                        finalScore += 6;
-                        break;
-                    case "Z": // Scissors gets defeated by Rock
-                        break;
+                    case "X" ->  finalScore += 3; // Tie
+                    case "Y" ->  finalScore += 6; // Paper defeats Rock
+                    case "Z" -> {} // Scissors gets defeated by Rock
                 }
             }
             case "B" -> {
                 switch (myChoice) {
-                    case "X": // Rock gets defeated by Paper
-                        break;
-                    case "Y": // Tie
-                        finalScore += 3;
-                        break;
-                    case "Z": // Scissors defeat Paper
-                        finalScore += 6;
-                        break;
+                    case "X" -> {} // Rock gets defeated by Paper
+                    case "Y" -> finalScore += 3; // Tie
+                    case "Z" -> finalScore += 6; // Scissors defeat Paper
                 }
             }
             case "C" -> {
                 switch (myChoice) {
-                    case "X": // Rock defeats Scissors
-                        finalScore += 6;
-                        break;
-                    case "Y": // Paper gets defeated by Scissors
-                        break;
-                    case "Z": // Tie
-                        finalScore += 3;
-                        break;
+                    case "X" -> finalScore += 6; // Rock defeats Scissors
+                    case "Y" -> {} // Paper gets defeated by Scissors
+                    case "Z" -> finalScore += 3; // Tie
+                }
+            }
+        }
+        return finalScore;
+    }
+
+    public int calculateScore2(String opponentChoice, String expectedOutcome) {
+        int finalScore = 0;
+        switch (expectedOutcome) {
+            case "X" -> { // Lose
+                switch (opponentChoice) {
+                    case "A" -> finalScore += 3;
+                    case "B" -> finalScore += 1;
+                    case "C" -> finalScore += 2;
+                }
+            }
+            case "Y" -> { // Draw
+                finalScore = 3;
+                switch (opponentChoice) {
+                    case "A" -> finalScore += 1;
+                    case "B" -> finalScore += 2;
+                    case "C" -> finalScore += 3;
+                }
+            }
+            case "Z" -> { // Win
+                finalScore = 6;
+                switch (opponentChoice) {
+                    case "A" -> finalScore += 2;
+                    case "B" -> finalScore += 3;
+                    case "C" -> finalScore += 1;
                 }
             }
         }
--- src/test/java/dev/codegeek/advent2022/Day2Test.java
+++ src/test/java/dev/codegeek/advent2022/Day2Test.java
@@ -6,10 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day2Test {
     @Test
-    void testFinalScore() {
+    void testCalculateScore1() {
         var day2 = new Day2();
-        assertEquals(8, day2.calculateScore("A", "Y"));
-        assertEquals(1, day2.calculateScore("B", "X"));
-        assertEquals(6, day2.calculateScore("C", "Z"));
+        assertEquals(8, day2.calculateScore1("A", "Y"));
+        assertEquals(1, day2.calculateScore1("B", "X"));
+        assertEquals(6, day2.calculateScore1("C", "Z"));
+    }
+
+    @Test
+    void testCalculateScore2() {
+        var day2 = new Day2();
+        assertEquals(4, day2.calculateScore2("A", "Y"));
+        assertEquals(1, day2.calculateScore2("B", "X"));
+        assertEquals(7, day2.calculateScore2("C", "Z"));
     }
 }
